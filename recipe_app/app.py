7de3ed4e009d3d8a54cb84b6ea780fa060962e64
Kyle Stanley <kstanley@ckmadvisors.com>
2019-04-26 16:17:40 -0400

    got it working

--- recipe_app/app.py
+++ recipe_app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import random
+from recipe_aggregator import get_ingredients
 
 app = Flask(__name__)
 
@@ -11,13 +12,31 @@ def index():
     if request.method == "POST":
     	add_recipe()
     else:
-        url = 'https://clutchpoints.com/wp-content/uploads/2019/04/Russell-Westbrook.jpg'
+        url = 'https://media.istockphoto.com/photos/empty-plate-with-knife-and-fork-on-white-kitchen-table-picture-id613022722?k=6&m=613022722&s=612x612&w=0&h=Fr-1qc4a04U0fI0wUlrTu1I-UeGycNNtMlN7pPPXAtc='
         return render_template('index.html', url=url)
 
 @app.route("/add_recipe", methods=["GET", "POST"])
 def add_recipe():
-    recipe = request.form['added_recipe']
-    return render_template('add_recipe.html', recipe=recipe)
+    try:
+        recipe_link = request.form['added_recipe_nyt']
+        recipe = get_ingredients(recipe_link, 'nyt')
+    except:
+        try:
+            recipe_link = request.form['added_recipe_epi']
+            recipe = get_ingredients(recipe_link, 'epi')
+        except:
+            try:
+                recipe_link = request.form['added_recipe_all']
+                recipe = get_ingredients(recipe_link, 'all')
+            except:
+                url = 'https://media.istockphoto.com/photos/empty-plate-with-knife-and-fork-on-white-kitchen-table-picture-id613022722?k=6&m=613022722&s=612x612&w=0&h=Fr-1qc4a04U0fI0wUlrTu1I-UeGycNNtMlN7pPPXAtc='
+                return render_template('index.html', url=url)
+    return render_template('add_recipe.html', recipe=recipe.to_html(classes='data', header="true"))
+
+@app.route("/recipe_sent", methods=["GET", "POST"])
+def recipe_sent():
+    email = request.form['recipe_sent']
+    return render_template('recipe_sent.html', email=email)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
---
+++ recipe_app/create_driver.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+
+class chrome_driver():
+    """Include test cases on a given url"""
+    def setUp(self):
+        """Start web driver"""
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+        self.driver = webdriver.Chrome(chrome_options=chrome_options)
+        self.driver.implicitly_wait(10)
+        return self.driver

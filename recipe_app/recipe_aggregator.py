0304200e7426472d8932490b22083d4eb02adb53
Kyle Stanley <Kyle Stanley>
2019-04-29 08:54:34 -0400

    add multiple recipes

--- recipe_app/recipe_aggregator.py
+++ recipe_app/recipe_aggregator.py
@@ -11,38 +11,40 @@ from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 from create_driver import chrome_driver
 
-def get_ingredients(link, site):
-    driver = chrome_driver().setUp()
-    driver.get(link)
-    if site == 'nyt':
-        driver.find_element_by_xpath('//*[@id="appContainer"]/div/div[2]/div/div/div/div[2]/div/div/div[1]/p/span').click()
-        time.sleep(1)
-        driver.find_element_by_name("userid").send_keys("kyle.m.stanley16@gmail.com")
-        driver.find_element_by_name("password").send_keys("1Baseball6")
-        driver.find_element_by_xpath('//*[@id="appContainer"]/div/div[2]/div/div/div/div[2]/div/div/div[3]/div[2]/form/div[2]/span').click()
-        nytimes_ingredient_list = {'quantity':[], 'ingredient':[]}
-        ingredients = driver.find_element_by_class_name("recipe-ingredients")
-        for ingredient in ingredients.find_elements_by_tag_name('li'):
-            nytimes_ingredient_list['quantity'].append(ingredient.find_element_by_class_name('quantity').text)
-            nytimes_ingredient_list['ingredient'].append(ingredient.find_element_by_class_name('ingredient-name').text)
-        df = pd.DataFrame(nytimes_ingredient_list)
-    if site == 'epi':
-        epicurious_ingredient_list = {'ingredient':[], 'quantity':[]}
-        driver.get(link)
-        ingredients = driver.find_element_by_class_name('ingredients')
-        for ingredient in ingredients.find_elements_by_class_name('ingredient'):
-            epicurious_ingredient_list['quantity'].append(ingredient.text.split(' ')[0])
-            epicurious_ingredient_list['ingredient'].append(' '.join(ingredient.text.split(' ')[1:]))
-        df = pd.DataFrame(epicurious_ingredient_list)
-    if site == 'all':
-        allrecipes_ingredient_list = {'ingredient':[], 'quantity':[]}
-        driver.get(link)
-        for ingredient in driver.find_elements_by_class_name('checkList__line'):
-            allrecipes_ingredient_list['ingredient'].append(' '.join(ingredient.text.split('\n')[0].split(' ')[1:]))
-            allrecipes_ingredient_list['quantity'].append(ingredient.text.split('\n')[0].split(' ')[0])
-        df = pd.DataFrame(allrecipes_ingredient_list)
-    return df
-# df = pd.concat([all_df, epi_df, ny_df], ignore_index=True)
+class get_ingredients():
+    def __init__(self):
+        self.driver = chrome_driver().setUp()
+        self.final_df = pd.DataFrame(columns=['quantity', 'ingredient'])
+
+    def scrape_ingredients(self, link, site):
+        self.driver.get(link)
+        if site == 'nyt':
+            self.driver.find_element_by_xpath('//*[@id="appContainer"]/div/div[2]/div/div/div/div[2]/div/div/div[1]/p/span').click()
+            time.sleep(1)
+            self.driver.find_element_by_name("userid").send_keys("kyle.m.stanley16@gmail.com")
+            self.driver.find_element_by_name("password").send_keys("1Baseball6")
+            self.driver.find_element_by_xpath('//*[@id="appContainer"]/div/div[2]/div/div/div/div[2]/div/div/div[3]/div[2]/form/div[2]/span').click()
+            nytimes_ingredient_list = {'quantity':[], 'ingredient':[]}
+            ingredients = self.driver.find_element_by_class_name("recipe-ingredients")
+            for ingredient in ingredients.find_elements_by_tag_name('li'):
+                nytimes_ingredient_list['quantity'].append(ingredient.find_element_by_class_name('quantity').text)
+                nytimes_ingredient_list['ingredient'].append(ingredient.find_element_by_class_name('ingredient-name').text)
+            self.final_df = self.final_df.append(pd.DataFrame(nytimes_ingredient_list), ignore_index=True)
+        if site == 'epi':
+            epicurious_ingredient_list = {'ingredient':[], 'quantity':[]}
+            self.driver.get(link)
+            ingredients = self.driver.find_element_by_class_name('ingredients')
+            for ingredient in ingredients.find_elements_by_class_name('ingredient'):
+                epicurious_ingredient_list['quantity'].append(ingredient.text.split(' ')[0])
+                epicurious_ingredient_list['ingredient'].append(' '.join(ingredient.text.split(' ')[1:]))
+            self.final_df = self.final_df.append(pd.DataFrame(epicurious_ingredient_list), ignore_index=True)
+        if site == 'all':
+            allrecipes_ingredient_list = {'ingredient':[], 'quantity':[]}
+            self.driver.get(link)
+            for ingredient in self.driver.find_elements_by_class_name('checkList__line'):
+                allrecipes_ingredient_list['ingredient'].append(' '.join(ingredient.text.split('\n')[0].split(' ')[1:]))
+                allrecipes_ingredient_list['quantity'].append(ingredient.text.split('\n')[0].split(' ')[0])
+            self.final_df = self.final_df.append(pd.DataFrame(allrecipes_ingredient_list), ignore_index=True)
 #
 # from sklearn.feature_extraction.text import CountVectorizer
 # vectorizer = CountVectorizer(stop_words='english', lowercase=True, strip_accents='ascii')
